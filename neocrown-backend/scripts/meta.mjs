7accffaff52e4569d62d4398bb121e7743a7e884
kenevoco <kenevoco@gmail.com>
2026-08-23 20:55:04 +0800

    meta.mjs

--- neocrown-backend/scripts/meta.mjs
+++ neocrown-backend/scripts/meta.mjs
@@ -1,6 +1,7 @@
 import path from 'path'
 import fs from 'fs'
 import { fileURLToPath } from 'url'
+import dayjs from 'dayjs'
 
 // ESM 兼容的 __dirname/__filename
 const __filename = fileURLToPath(import.meta.url)
@@ -9,7 +10,7 @@ const __dirname = path.dirname(__filename)
 const distfile = path.resolve(__dirname, '../public/meta.json')
 
 const content = {
-  deployTime: new Date().toLocaleString()
+  deployTime: dayjs().format('YYYY-MM-DD HH:mm:ss'),
 }
 fs.writeFileSync(distfile, JSON.stringify(content, null, 2))
 
